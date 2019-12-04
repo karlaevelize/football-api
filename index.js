b2ab874f620dc57b3eb33af0159fe31de46a53ca
karaevelize <k.evelize@gmail.com>
2019-12-04 14:21:01 +0100

    SQL + Sequelize

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 4001;
+const db = require("./db");
 
 app.listen(port, () =>
   console.log(`Hello there!, I am listening on port ${port}!`)
