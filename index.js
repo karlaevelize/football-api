fc4e168df01d515f196f00466848efb13fcef1ce
karaevelize <k.evelize@gmail.com>
2019-12-04 14:53:59 +0100

    Schema

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 4001;
+const port = process.env.PORT || 4005;
 const db = require("./db");
+const Team = require("./team/model");
 
 app.listen(port, () =>
   console.log(`Hello there!, I am listening on port ${port}!`)
